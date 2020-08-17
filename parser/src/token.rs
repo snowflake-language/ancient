a3e0f0d25b41f3343deb42a233b996abb61437d2
brecert <11599528+Brecert@users.noreply.github.com>
2020-08-17 09:14:43 -0400

    initial parser commit this commit message is a non-detailed message because the additions should be self explanatory, and [some] comments exist in this commit and can be used in place of a proper commit message. all tests should pass.

---
+++ parser/src/token.rs
@@ -0,0 +1,120 @@
+use logos::Logos;
+use num_bigint::BigInt;
+
+fn lex_char(lex: &mut logos::Lexer<Token>) -> Option<char> {
+    lex.source().chars().nth(lex.span().start)
+}
+
+#[derive(Logos, Clone, Debug, PartialEq)]
+pub enum Token {
+    #[regex("[a-zA-Z][a-zA-Z1-9_]*", |lex| lex.slice().parse())]
+    Identifier(String),
+
+    #[regex(r"[0-9]*\.[0-9]+", |lex| lex.slice().parse())]
+    Float(f64),
+
+    #[regex("[0-9][0-9_]*", |lex| lex.slice().parse())]
+    Integer(BigInt),
+
+    // should be replaced with inserted tokens
+    #[regex("\n(  )*", |lex| ((lex.slice().len() - 1) / 2))]
+    Indentation(usize),
+
+    // todo: eventually give proper names to some of these
+    // "non-symbol" character combinations
+    #[token("=")]
+    Equal,
+
+    #[token("::")]
+    ColonColon,
+
+    #[token("**")]
+    StarStar,
+
+    #[token("=>")]
+    LargeArrowRight,
+
+    #[token("->")]
+    SmallArrowRight,
+
+    #[regex(r"\s", logos::skip)]
+    Whitespace,
+
+    #[regex(r"[!-/:-@\[-`{-~()]", lex_char)]
+    Symbol(char),
+
+    #[error(|lex| lex.slice().parse())]
+    Unknown,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use Token::*;
+
+    #[test]
+    fn lex_test() {
+        let source = indoc! {"
+            fib :: isize -> isize
+            fib n =>
+              (fib n - 1) + (fib n - 2)
+        "};
+        let tokens: Vec<_> = Token::lexer(source).collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Identifier(String::from("fib")),
+                ColonColon,
+                Identifier(String::from("isize")),
+                SmallArrowRight,
+                Identifier(String::from("isize")),
+                Indentation(0),
+                Identifier(String::from("fib")),
+                Identifier(String::from("n")),
+                LargeArrowRight,
+                Indentation(1),
+                Symbol('('),
+                Identifier(String::from("fib")),
+                Identifier(String::from("n")),
+                Symbol('-'),
+                Integer(BigInt::from(1)),
+                Symbol(')'),
+                Symbol('+'),
+                Symbol('('),
+                Identifier(String::from("fib")),
+                Identifier(String::from("n")),
+                Symbol('-'),
+                Integer(BigInt::from(2)),
+                Symbol(')'),
+                // the final newline is from indoc
+                Indentation(0)
+            ]
+        )
+    }
+
+    #[test]
+    fn lex_indent_test() {
+        let source = indoc! {"
+            block =>
+              123
+              abc
+              123
+        "};
+        let tokens: Vec<_> = Token::lexer(source).collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Identifier(String::from("block")),
+                LargeArrowRight,
+                Indentation(1),
+                Integer(BigInt::from(123)),
+                Indentation(1),
+                Identifier(String::from("abc")),
+                Indentation(1),
+                Integer(BigInt::from(123)),
+                Indentation(0),
+            ]
+        )
+    }
+}
