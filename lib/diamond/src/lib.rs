c4641f7ae006a534c510dbd7f58021abcdb60965
Skylar Bleed <monarrk@aaathats3as.com>
2020-08-21 14:32:51 -0400

    Add branches for every variant

--- lib/diamond/src/lib.rs
+++ lib/diamond/src/lib.rs
@@ -1,9 +1,60 @@
-use parser::ast::Statement;
+use parser::ast::{Expression, Type, OpSymbol, Statement};
 
-fn eval(statement: Statement) {
+fn eval_statement(statement: Statement) {
     match statement {
         Statement::FnDecl { name: n, args: a, body: b } => {
             
         },
+
+        Statement::TypeDecl { name: n, body: b } => {
+            match b {
+                Type::FnSig { args: a, ret: r } => {
+                    
+                },
+                Type::Nat(b) => {
+
+                },
+                Type::Identifier(s) => {
+
+                },
+            };
+        },
+
+        Statement::Expression(e) => {
+            match e {
+                Expression::OpCall { op: o, args: a } => {
+                    match o {
+                        OpSymbol::Plus => {
+
+                        },
+                        OpSymbol::Minus => {
+
+                        },
+                        OpSymbol::Star => {
+
+                        },
+                        OpSymbol::ForwardSlash => {
+
+                        },
+                    };
+                },
+
+                Expression::FnCall { name: n, args: a } => {
+
+                },
+
+                Expression::Integer(i) => {
+
+                },
+
+                Expression::Identifier(i) => {
+
+                },
+            };
+        },
     };
+}
+
+pub fn eval(statement: Statement) {
+    eval_statement(statement);
 }
